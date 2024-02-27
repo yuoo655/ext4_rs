542abeb0fa0ea3c64ed346d3971158225f5a93b4
yuoo655 <48611913+yuoo655@users.noreply.github.com>
2024-02-27 15:53:20 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,10 +1,10 @@
 rm -rf ex4.img
 dd if=/dev/zero of=ex4.img bs=1M count=1024
 mkfs.ext4 ./ex4.img
-mount ./ex4.img ./tmp/
+sudo mount ./ex4.img ./tmp/
 cd tmp
-mkdir -p test_files
-cp ../test_files/* ./test_files/
+sudo mkdir -p test_files
+sudo cp ../test_files/* ./test_files/
 cd ../
-umount tmp
+sudo umount tmp
 cargo run
