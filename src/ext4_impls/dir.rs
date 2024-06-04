4a0646dfc5b4e30a0d41719d3215b793e5dddbea
yuoo655 <lenuulan@gmail.com>
2024-06-04 21:25:41 +0800

    docs: add docs for dir interface

--- src/ext4_impls/dir.rs
+++ src/ext4_impls/dir.rs
@@ -6,28 +6,43 @@ use crate::ext4_defs::*;
 
 
 impl Ext4 {
+    /// 
     pub fn dir_find_entry(
         &self,
-        parent: &mut Ext4InodeRef,
+        parent_inode: u32,
         name: &str,
         result: &mut Ext4DirSearchResult,
     ) -> Result<usize> {
+
+        // load parent inode
+        let parent = self.get_inode_ref(parent_inode);
+        assert!(parent.inode.is_dir());
+
+        // start from the first logical block
         let mut iblock = 0;
+        // physical block id
         let mut fblock: Ext4Fsblk = 0;
 
+        // calculate total blocks
         let inode_size: u64 = parent.inode.size();
         let total_blocks: u64 = inode_size / BLOCK_SIZE as u64;
 
+        // iterate all blocks
         while iblock < total_blocks {
             let search_path = self.find_extent(&parent, iblock as u32);
 
             if let Ok(path) = search_path {
+                // get the last path
                 let path = path.path.last().unwrap();
+
+                // get physical block id
                 fblock = path.pblock;
-                // load_block
+
+                // load physical block
                 let mut ext4block =
                     Block::load(self.block_device.clone(), fblock as usize * BLOCK_SIZE);
 
+                // find entry in block
                 let r = self.dir_find_in_block(&mut ext4block, name);
 
                 if r.is_ok() {
@@ -37,38 +52,63 @@ impl Ext4 {
             } else {
                 return_errno_with_message!(Errno::ENOENT, "dir search fail")
             }
-
+            // go to next block
             iblock += 1
         }
 
         return_errno_with_message!(Errno::ENOENT, "dir search fail");
     }
 
+
+    /// Find a directory entry in a block
     pub fn dir_find_in_block(&self, block: &Block, name: &str) -> Result<Ext4DirEntry> {
         let mut offset = 0;
+
+        // start from the first entry
         while offset < BLOCK_SIZE - core::mem::size_of::<Ext4DirEntryTail>() {
             let de: Ext4DirEntry = block.read_offset_as(offset);
             if !de.unused() && de.compare_name(name) {
                 return Ok(de);
             }
+            // go to next entry
             offset += de.entry_len() as usize;
         }
         return_errno_with_message!(Errno::ENOENT, "dir find in block failed");
     }
 
+    /// Get dir entries of a inode
     pub fn dir_get_entries(&self, inode: u32) -> Vec<Ext4DirEntry>{
         let mut entries = Vec::new();
+
+        // load inode
         let inode_ref = self.get_inode_ref(inode);
+        assert!(inode_ref.inode.is_dir());
+
+        // calculate total blocks
         let inode_size = inode_ref.inode.size();
         let total_blocks = inode_size / BLOCK_SIZE as u64;
+
+        // start from the first logical block
         let mut iblock = 0;
+
+        // iterate all blocks
         while iblock < total_blocks{
+
+            // get physical block id of a logical block id
             let search_path = self.find_extent(&inode_ref, iblock as u32);
+
             if let Ok(path) = search_path {
+                // get the last path
                 let path = path.path.last().unwrap();
+
+                // get physical block id
                 let fblock = path.pblock;
+
+                // load physical block
                 let ext4block = Block::load(self.block_device.clone(), fblock as usize * BLOCK_SIZE);
                 let mut offset = 0;
+
+                // iterate all entries in a block
                 while offset < BLOCK_SIZE - core::mem::size_of::<Ext4DirEntryTail>() {
                     let de: Ext4DirEntry = ext4block.read_offset_as(offset);
                     if !de.unused() {
@@ -77,13 +117,15 @@ impl Ext4 {
                     offset += de.entry_len() as usize;
                 }
             }
+
+            // go ot next block
             iblock += 1;
         }
         entries
     }
 
 
-    pub fn get_pblock_idx(&self, lblock: Ext4Lblk) -> Result<Ext4Fsblk> {
+    pub fn get_pblock_idx(&self, inode_ref:&Ext4InodeRef , lblock: Ext4Lblk) -> Result<Ext4Fsblk> {
         return_errno_with_message!(Errno::ENOENT, "file not found");
     }
 }
