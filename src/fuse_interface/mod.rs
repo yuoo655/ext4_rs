c032bc07720e5b89b1c4be3f86584bc27faa50f1
yuoo655 <lenuulan@gmail.com>
2024-06-06 22:26:24 +0800

    style: move all unimpl fns to the end

--- src/fuse_interface/mod.rs
+++ src/fuse_interface/mod.rs
@@ -7,19 +7,6 @@ use crate::utils::path_check;
 
 /// fuser interface for ext4
 impl Ext4 {
-    /// Initialize filesystem.
-    /// Called before any other filesystem method.
-    /// The kernel module connection can be configured using the KernelConfig object
-    fn fuse_init(&mut self) -> Result<usize> {
-        Ok(EOK)
-    }
-
-    /// Clean up filesystem.
-    /// Called on filesystem exit.
-    fn fuse_destroy(&mut self) -> Result<usize> {
-        Ok(EOK)
-    }
-
     /// Look up a directory entry by name and get its attributes.
     pub fn fuse_lookup(&self, parent: u64, name: &str) -> Result<FileAttr> {
         let mut search_result = Ext4DirSearchResult::new(Ext4DirEntry::default());
@@ -123,7 +110,6 @@ impl Ext4 {
         let read_size = self.read_at(ino as u32, 0, &mut read_buf)?;
         Ok(read_buf)
     }
-
     /// Create file node.
     /// Create a regular file, character device, block device, fifo or socket node.
     fn fuse_mknod(
@@ -142,7 +128,6 @@ impl Ext4 {
         let inode_ref = self.create(parent as u32, name, mode as u16)?;
         Ok(EOK)
     }
-
     /// Create a directory.
     fn fuse_mkdir(&mut self, parent: u64, name: &str, mode: u32, umask: u32) -> Result<usize> {
         let mut search_result = Ext4DirSearchResult::new(Ext4DirEntry::default());
@@ -158,7 +143,6 @@ impl Ext4 {
         let inode_ref = self.create(parent as u32, name, mode as u16)?;
         Ok(EOK)
     }
-
     /// Remove a file.
     fn fuse_unlink(&mut self, parent: u64, name: &str) -> Result<usize> {
         // unlink actual remove a file
@@ -190,7 +174,6 @@ impl Ext4 {
 
         Ok(EOK)
     }
-
     /// Remove a directory.
     fn fuse_rmdir(&mut self, parent: u64, name: &str) -> Result<usize> {
         let mut search_result = Ext4DirSearchResult::new(Ext4DirEntry::default());
@@ -213,7 +196,6 @@ impl Ext4 {
 
         return Ok(EOK);
     }
-
     /// Create a symbolic link.
     fn fuse_symlink(&mut self, parent: u64, link_name: &str, target: &str) -> Result<usize> {
         let mut search_result = Ext4DirSearchResult::new(Ext4DirEntry::default());
@@ -229,10 +211,6 @@ impl Ext4 {
         let inode_ref = self.create(parent as u32, link_name, mode as u16)?;
         Ok(EOK)
     }
-
-    /// Rename a file.
-    fn fuse_rename(&mut self, parent: u64, name: &str, newparent: u64, newname: &str, flags: u32) {}
-
     /// Create a hard link.
     /// Params:
     /// ino: the inode number of the source file
@@ -344,42 +322,6 @@ impl Ext4 {
         Ok(write_size)
     }
 
-    /// Flush method.
-    /// This is called on each close() of the opened file. Since file descriptors can
-    /// be duplicated (dup, dup2, fork), for one open call there may be many flush
-    /// calls. Filesystems shouldn't assume that flush will always be called after some
-    /// writes, or that if will be called at all. fh will contain the value set by the
-    /// open method, or will be undefined if the open method didn't set any value.
-    /// NOTE: the name of the method is misleading, since (unlike fsync) the filesystem
-    /// is not forced to flush pending writes. One reason to flush data, is if the
-    /// filesystem wants to return write errors. If the filesystem supports file locking
-    /// operations (setlk, getlk) it should remove all locks belonging to 'lock_owner'.
-    fn fuse_flush(&mut self, ino: u64, fh: u64, lock_owner: u64) {}
-
-    /// Release an open file.
-    /// Release is called when there are no more references to an open file: all file
-    /// descriptors are closed and all memory mappings are unmapped. For every open
-    /// call there will be exactly one release call. The filesystem may reply with an
-    /// error, but error values are not returned to close() or munmap() which triggered
-    /// the release. fh will contain the value set by the open method, or will be undefined
-    /// if the open method didn't set any value. flags will contain the same flags as for
-    /// open.
-    fn fuse_release(
-        &mut self,
-
-        _ino: u64,
-        _fh: u64,
-        _flags: i32,
-        _lock_owner: Option<u64>,
-        _flush: bool,
-    ) {
-    }
-
-    /// Synchronize file contents.
-    /// If the datasync parameter is non-zero, then only the user data should be flushed,
-    /// not the meta data.
-    fn fuse_fsync(&mut self, ino: u64, fh: u64, datasync: bool) {}
-
     /// Open a directory.
     /// Filesystem may store an arbitrary file handle (pointer, index, etc) in fh, and
     /// use this in other all other directory stream operations (readdir, releasedir,
@@ -415,52 +357,6 @@ impl Ext4 {
         entries = entries[offset as usize..].to_vec();
         Ok(entries)
     }
-
-    /// Read directory.
-    /// Send a buffer filled using buffer.fill(), with size not exceeding the
-    /// requested size. Send an empty buffer on end of stream. fh will contain the
-    /// value set by the opendir method, or will be undefined if the opendir method
-    /// didn't set any value.
-    fn fuse_readdirplus(&mut self, ino: u64, fh: u64, offset: i64) {}
-
-    /// Release an open directory.
-    /// For every opendir call there will be exactly one releasedir call. fh will
-    /// contain the value set by the opendir method, or will be undefined if the
-    /// opendir method didn't set any value.
-    fn fuse_releasedir(&mut self, _ino: u64, _fh: u64, _flags: i32) {}
-
-    /// Synchronize directory contents.
-    /// If the datasync parameter is set, then only the directory contents should
-    /// be flushed, not the meta data. fh will contain the value set by the opendir
-    /// method, or will be undefined if the opendir method didn't set any value.
-    fn fuse_fsyncdir(&mut self, ino: u64, fh: u64, datasync: bool) {}
-
-    /// Get file system statistics.
-    fn fuse_statfs(&mut self, _ino: u64) {}
-
-    /// Set an extended attribute.
-    fn fuse_setxattr(&mut self, ino: u64, name: &str, _value: &[u8], flags: i32, position: u32) {}
-
-    /// Get an extended attribute.
-    /// If `size` is 0, the size of the value should be sent with `reply.size()`.
-    /// If `size` is not 0, and the value fits, send it with `reply.data()`, or
-    /// `reply.error(ERANGE)` if it doesn't.
-    fn fuse_getxattr(&mut self, ino: u64, name: &str, size: u32) {}
-
-    /// List extended attribute names.
-    /// If `size` is 0, the size of the value should be sent with `reply.size()`.
-    /// If `size` is not 0, and the value fits, send it with `reply.data()`, or
-    /// `reply.error(ERANGE)` if it doesn't.
-    fn fuse_listxattr(&mut self, ino: u64, size: u32) {}
-
-    /// Remove an extended attribute.
-    fn fuse_removexattr(&mut self, ino: u64, name: &str) {}
-
-    /// Check file access permissions.
-    /// This will be called for the access() system call. If the 'default_permissions'
-    /// mount option is given, this method is not called. This method is not called
-    /// under Linux kernel versions 2.4.x
-    fn fuse_access(&mut self, ino: u64, mask: i32) {}
 
     /// Create and open a file.
     /// If the file does not exist, first create it with the specified mode, and then
@@ -523,6 +419,128 @@ impl Ext4 {
         Ok(EOK)
     }
 
+    /// Initialize filesystem.
+    /// Called before any other filesystem method.
+    /// The kernel module connection can be configured using the KernelConfig object
+    fn fuse_init(&mut self) -> Result<usize> {
+        Ok(EOK)
+    }
+
+    /// Clean up filesystem.
+    /// Called on filesystem exit.
+    fn fuse_destroy(&mut self) -> Result<usize> {
+        Ok(EOK)
+    }
+
+    /// Rename a file.
+    fn fuse_rename(&mut self, parent: u64, name: &str, newparent: u64, newname: &str, flags: u32) {
+        unimplemented!();
+    }
+
+    /// Flush method.
+    /// This is called on each close() of the opened file. Since file descriptors can
+    /// be duplicated (dup, dup2, fork), for one open call there may be many flush
+    /// calls. Filesystems shouldn't assume that flush will always be called after some
+    /// writes, or that if will be called at all. fh will contain the value set by the
+    /// open method, or will be undefined if the open method didn't set any value.
+    /// NOTE: the name of the method is misleading, since (unlike fsync) the filesystem
+    /// is not forced to flush pending writes. One reason to flush data, is if the
+    /// filesystem wants to return write errors. If the filesystem supports file locking
+    /// operations (setlk, getlk) it should remove all locks belonging to 'lock_owner'.
+    fn fuse_flush(&mut self, ino: u64, fh: u64, lock_owner: u64) {
+        unimplemented!();
+    }
+
+    /// Release an open file.
+    /// Release is called when there are no more references to an open file: all file
+    /// descriptors are closed and all memory mappings are unmapped. For every open
+    /// call there will be exactly one release call. The filesystem may reply with an
+    /// error, but error values are not returned to close() or munmap() which triggered
+    /// the release. fh will contain the value set by the open method, or will be undefined
+    /// if the open method didn't set any value. flags will contain the same flags as for
+    /// open.
+    fn fuse_release(
+        &mut self,
+        _ino: u64,
+        _fh: u64,
+        _flags: i32,
+        _lock_owner: Option<u64>,
+        _flush: bool,
+    ) {
+        unimplemented!();
+    }
+
+    /// Synchronize file contents.
+    /// If the datasync parameter is non-zero, then only the user data should be flushed,
+    /// not the meta data.
+    fn fuse_fsync(&mut self, ino: u64, fh: u64, datasync: bool) {
+        unimplemented!();
+    }
+
+    /// Read directory.
+    /// Send a buffer filled using buffer.fill(), with size not exceeding the
+    /// requested size. Send an empty buffer on end of stream. fh will contain the
+    /// value set by the opendir method, or will be undefined if the opendir method
+    /// didn't set any value.
+    fn fuse_readdirplus(&mut self, ino: u64, fh: u64, offset: i64) {
+        unimplemented!();
+    }
+
+    /// Release an open directory.
+    /// For every opendir call there will be exactly one releasedir call. fh will
+    /// contain the value set by the opendir method, or will be undefined if the
+    /// opendir method didn't set any value.
+    fn fuse_releasedir(&mut self, _ino: u64, _fh: u64, _flags: i32) {
+        unimplemented!();
+    }
+
+    /// Synchronize directory contents.
+    /// If the datasync parameter is set, then only the directory contents should
+    /// be flushed, not the meta data. fh will contain the value set by the opendir
+    /// method, or will be undefined if the opendir method didn't set any value.
+    fn fuse_fsyncdir(&mut self, ino: u64, fh: u64, datasync: bool) {
+        unimplemented!();
+    }
+
+    /// Get file system statistics.
+    fn fuse_statfs(&mut self, _ino: u64) {
+        unimplemented!();
+    }
+
+    /// Set an extended attribute.
+    fn fuse_setxattr(&mut self, ino: u64, name: &str, _value: &[u8], flags: i32, position: u32) {
+        unimplemented!();
+    }
+
+    /// Get an extended attribute.
+    /// If `size` is 0, the size of the value should be sent with `reply.size()`.
+    /// If `size` is not 0, and the value fits, send it with `reply.data()`, or
+    /// `reply.error(ERANGE)` if it doesn't.
+    fn fuse_getxattr(&mut self, ino: u64, name: &str, size: u32) {
+        unimplemented!();
+    }
+
+    /// List extended attribute names.
+    /// If `size` is 0, the size of the value should be sent with `reply.size()`.
+    /// If `size` is not 0, and the value fits, send it with `reply.data()`, or
+    /// `reply.error(ERANGE)` if it doesn't.
+    fn fuse_listxattr(&mut self, ino: u64, size: u32) {
+        unimplemented!();
+    }
+
+    /// Remove an extended attribute.
+    fn fuse_removexattr(&mut self, ino: u64, name: &str) {
+        unimplemented!();
+    }
+
+    /// Check file access permissions.
+    /// This will be called for the access() system call. If the 'default_permissions'
+    /// mount option is given, this method is not called. This method is not called
+    /// under Linux kernel versions 2.4.x
+    fn fuse_access(&mut self, ino: u64, mask: i32) {
+        unimplemented!();
+    }
+
     /// Test for a POSIX file lock.
     fn fuse_getlk(
         &mut self,
@@ -534,6 +552,7 @@ impl Ext4 {
         typ: i32,
         pid: u32,
     ) {
+        unimplemented!();
     }
 
     /// Acquire, modify or release a POSIX file lock.
@@ -554,12 +573,15 @@ impl Ext4 {
         pid: u32,
         sleep: bool,
     ) {
+        unimplemented!();
     }
 
     /// Map block index within file to block index within device.
     /// Note: This makes sense only for block device backed filesystems mounted
     /// with the 'blkdev' option
-    fn fuse_bmap(&mut self, ino: u64, blocksize: u32, idx: u64) {}
+    fn fuse_bmap(&mut self, ino: u64, blocksize: u32, idx: u64) {
+        unimplemented!();
+    }
 
     /// control device
     fn fuse_ioctl(
@@ -571,6 +593,7 @@ impl Ext4 {
         in_data: &[u8],
         out_size: u32,
     ) {
+        unimplemented!();
     }
 
     /// Poll for events
@@ -586,10 +609,14 @@ impl Ext4 {
     // }
 
     /// Preallocate or deallocate space to a file
-    fn fuse_fallocate(&mut self, ino: u64, fh: u64, offset: i64, length: i64, mode: i32) {}
+    fn fuse_fallocate(&mut self, ino: u64, fh: u64, offset: i64, length: i64, mode: i32) {
+        unimplemented!();
+    }
 
     /// Reposition read/write file offset
-    fn fuse_lseek(&mut self, ino: u64, fh: u64, offset: i64, whence: i32) {}
+    fn fuse_lseek(&mut self, ino: u64, fh: u64, offset: i64, whence: i32) {
+        unimplemented!();
+    }
 
     /// Copy the specified range from the source inode to the destination inode
     fn fuse_copy_file_range(
@@ -603,5 +630,6 @@ impl Ext4 {
         len: u64,
         flags: u32,
     ) {
+        unimplemented!();
     }
 }
